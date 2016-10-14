b724c0c0e8d3c6029b0dbfb1433dd6ac519c7c1d
Anderson Luix <Anderson Luiz@DESKTOP-PMPNJL0>
2016-10-14 08:40:54 -0300

    ajustes

--- src/main/java/br/com/panoramico/acesso/util/Formatacao.java
+++ src/main/java/br/com/panoramico/acesso/util/Formatacao.java
@@ -8,6 +8,7 @@ package br.com.panoramico.acesso.util;
 
 
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
